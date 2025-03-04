e9722136b476b18a91903c99e5b81b64e06c8d0b
alexbowes51 <alexbowes12@outlook.ie>
2025-03-04 00:19:14 +0000

    fixed shooting mechanic

--- Scenes/enemys/Ammo/arrow.gd
+++ Scenes/enemys/Ammo/arrow.gd
@@ -2,15 +2,34 @@ extends CharacterBody2D
 
 var direction : Vector2 = Vector2.ZERO
 var speed : float = 100
+func _ready():
+	velocity = direction * speed
+	 
 
 func _physics_process(delta):
-	velocity = direction * speed
 	move_and_slide()
 
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
+func set_direction(new_dir:Vector2):
+	direction = new_dir
+	velocity = direction * speed 
+	rotation = direction.angle()
+	
+	
+
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("Player"):
 		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+
+func _on_arrow_hit_bow_enemy_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area && area.name == "Player_HitBox":
+		self.queue_free()
+
+func enemy_arrow():
+	pass
--- Scenes/enemys/Cross_Bow_Soldier/cross_bow_soldier.gd
+++ Scenes/enemys/Cross_Bow_Soldier/cross_bow_soldier.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var spawn_arrow: Node2D = $Spawn_arrow
+
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var fire_timer: Timer = $Fire_Timer
@@ -21,7 +21,8 @@ func aim():
 	if player != null:
 		ray_cast_2d.target_position = to_global(player.position)
 		look_at(player.get_global_position())
-		spawn_arrow.position = (player.global_position - position).normalized()
+		global_rotation_degrees += -90
+		
 	
 	
 
@@ -39,6 +40,7 @@ func Cross_Bow_Soldier():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("Player"):
 		player = body
+		
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("Player"):
@@ -51,11 +53,14 @@ func _on_fire_timer_timeout() -> void:
 	
 	
 func shoot():
-	if player != null && spawn_arrow != null :
+	if player != null:
 		var bullet = ammo.instantiate()
 		get_parent().get_parent().add_child(bullet)
-		bullet.position = spawn_arrow.global_position
-		bullet.direction = (player.global_position - position).normalized()
+		bullet.position = position
+		var shooting_dir = (player.global_position - global_position).normalized()
+		bullet.set_direction(shooting_dir)
+		bullet.global_rotation -= 140
+	
 
 	
 	
