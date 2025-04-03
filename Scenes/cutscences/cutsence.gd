756b83945744875e37b833b9d5c69a0c86d46bfa
alexbowes51 <alexbowes12@outlook.ie>
2025-04-03 23:15:39 +0100

    started intro cutscene

---
+++ Scenes/cutscences/cutsence.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@onready var camera_2d: Camera2D = $Path2D/PathFollow2D/Camera2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
+
+
+
+var path_follow = false
+var open_scene = false
+var played = false
+var played2 = false
+func ready():
+	cutscenceopening()
+	
+func cutscenceopening():
+	
+	if !played:
+		played = true
+		animation_player.play("fade_in")
+		open_scene = true
+		path_follow = true
+		camera_2d.enabled = true
+	
+func cutsceneclose():
+	
+	if !played2:
+		played2 = true
+		animation_player.play("Fade_out")
+		open_scene = false
+		path_follow = false
+		camera_2d.enabled = false
+	
+	
+func _process(delta: float) -> void:
+		cutscenceopening()
+		if path_follow_2d:
+			path_follow_2d.progress_ratio += 0.00025
+		
+			
+			if path_follow_2d.progress_ratio >= 0.97:
+				cutsceneclose()
